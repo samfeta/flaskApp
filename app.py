3febb7ea16f24f6e33de23931daee9b492e22c41
samfeta <mr.samfeta@gmail.com>
2022-06-04 10:10:00 -0400

    v1

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def  home():
+    return "<h1> Demo Flask App is working in version 1v selman</h1>"
+
+if __name__== '__main__':
+    app.run(debug=True,host="0.0.0.0")
+    
